d701d244ca8c8d2ae21866beb90aca415c41e331
Kuba <staniojakub@gmail.com>
2022-06-20 23:44:30 +0300

    chore(fe): install and configure DaisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite-plugin-windicss';
+import * as daisyUI from 'daisyui';
 
 export default defineConfig({
   darkMode: false,
+  plugins: [daisyUI],
   theme: {
     extend: {},
   },
