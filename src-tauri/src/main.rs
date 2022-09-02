d4cebac62e9fed81605953a340b49a4d3ff6032b
Kuba <staniojakub@gmail.com>
2022-09-03 01:12:10 +0300

    fix(*): add native menu options - enable functions like copy, paste, quit with keyboard support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,25 +7,36 @@ mod cg;
 
 use cg::api;
 use cg::models::{CoinData, SearchResult};
-use tauri::{CustomMenuItem, Menu, Submenu};
+use tauri::{Menu, MenuItem, Submenu};
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let close = CustomMenuItem::new("close".to_string(), "Close");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    let menu = Menu::new().add_submenu(submenu);
+    // https://github.com/tauri-apps/tauri/issues/1055#issuecomment-1008895581
+    let about_menu = Submenu::new(
+        "App",
+        Menu::new()
+            .add_native_item(MenuItem::Hide)
+            .add_native_item(MenuItem::HideOthers)
+            .add_native_item(MenuItem::ShowAll)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Quit),
+    );
+
+    let edit_menu = Submenu::new(
+        "Edit",
+        Menu::new()
+            .add_native_item(MenuItem::Undo)
+            .add_native_item(MenuItem::Redo)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Cut)
+            .add_native_item(MenuItem::Copy)
+            .add_native_item(MenuItem::Paste)
+            .add_native_item(MenuItem::SelectAll),
+    );
+
+    let menu = Menu::new().add_submenu(about_menu).add_submenu(edit_menu);
 
     tauri::Builder::default()
         .menu(menu)
-        .on_menu_event(|event| match event.menu_item_id() {
-            "quit" => {
-                std::process::exit(0);
-            }
-            "close" => {
-                event.window().close().unwrap();
-            }
-            _ => {}
-        })
         .invoke_handler(tauri::generate_handler![get_coin_data, search_coins])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
